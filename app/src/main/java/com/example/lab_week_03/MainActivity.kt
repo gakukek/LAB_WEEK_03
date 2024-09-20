108bc994d169eb5341a1c109af93bd9870dac4e5
gakukek <darrylclaylayaya@gmail.com>
2024-09-20 15:20:05 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lab_week_03/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.lab_week_03
+
+import android.content.ContentValues.TAG
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentContainerView
+
+
+class MainActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+    }
+}
